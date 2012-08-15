f2d4293554b6213a74b3050f5e6416c01828da1e
Jason Whittle <jason.whittle@gmail.com>
2012-08-15 17:19:30 -0400

    Update dependencies to match yesod-platform-1.1.

--- Text/XML/Stream/Parse/Skip.hs
+++ Text/XML/Stream/Parse/Skip.hs
@@ -1,26 +1,28 @@
+{-# LANGUAGE PatternGuards #-}
 module Text.XML.Stream.Parse.Skip
        ( skipTag
        , skipContents
        , readM
        ) where
 
-import Data.Conduit
+import Data.Conduit (MonadThrow, Sink, Pipe, await)
 import Data.Text (Text, unpack)
 import Text.XML.Stream.Parse
 import Data.XML.Types
 
-skipTag :: (MonadThrow m) => Text -> Sink Event m (Maybe ())
+skipTag :: MonadThrow m => Text -> Sink Event m (Maybe ())
 skipTag n = tagPredicate ((== n) . nameLocalName) ignoreAttrs $ const $ many (skipContents n) >> return ()
 
+skipContents :: Monad m => Text -> Pipe l Event o u m (Maybe ())
 skipContents t = do
   x <- await
   case x of
-    Just (EventEndElement n) | nameLocalName n == t -> Done x Nothing
-    Nothing -> Done x Nothing
+    Just (EventEndElement n) | nameLocalName n == t -> return Nothing
+    Nothing -> return Nothing
     _ -> return (Just ())
 
 readM :: (Monad m) => Text -> m Integer
 readM t | [x] <- parse = return x
         | otherwise = fail $ "Failed to parse \"" ++ s ++ "\" as an Integer."
   where s = unpack t
-        parse = [x | (x,_) <- reads s]
+        parse = [x | (x, _) <- reads s]
